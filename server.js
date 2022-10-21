0c8e999e7c02cc04d289fb9a50f86f21118d8d10
Hugo Deiró <hugodeiro@gmail.com>
2022-10-21 11:37:51 -0300

    Adding grpc server side

--- server.js
+++ server.js
@@ -1,3 +1,39 @@
 const grpc = require('grpc');
 const protoLoader = require('@grpc/proto-loader');
 
+// Gets the definition of package from the given protobuf file
+const packageDef = protoLoader.loadSync('todo.proto', {});
+
+// Load the protobuf definition on a grpc model frm it's definition
+const grpcObject = grpc.loadPackageDefinition(packageDef);
+
+// Gets the todo package (defined in protobuf file)
+const { todoPackage } = grpcObject;
+
+// Creates server
+const server = new grpc.Server();
+server.bind(
+    '0.0.0.0:8080', // address:port (binds to anything without the need to define a protocol)
+    grpc.ServerCredentials.createInsecure() // Creating insecure conection
+);
+
+// Adds services
+server.addService(todoPackage.Todo.service, {
+    'createTodo': createTodo,
+    'readTodos': readTodos
+});
+
+// Functions to be listened by grpc services
+// Tooks two params always: 
+//      - call     // similar to a Request object
+//      - callback // Will execute with a response from the grpc service
+function createTodo(call, callback) {
+    console.log(call);
+}
+
+function readTodos(call, callback) {
+
+}
+
+// Start service
+server.start();
